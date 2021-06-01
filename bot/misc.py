193daf3a4493bcdb312e10cdebc65a2428716ec0
Benyamin Ginzburg <benyomin.94@gmail.com>
2021-06-01 15:00:09 +0300

    Update misc.py

--- bot/misc.py
+++ bot/misc.py
@@ -6,7 +6,7 @@ from aiogram import Bot, Dispatcher, types
 from .config import BOT_TOKEN
 
 
-logger = bl.getLogger('zmanim_bot')
+logger = bl.getLogger('my_bot')
 
 loop = asyncio.get_event_loop()
 bot = Bot(BOT_TOKEN, loop, parse_mode=types.ParseMode.HTML)
