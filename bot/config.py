33747669a8c05658e4310b4d5005f65c905d1bbb
Benyamin Ginzburg <benyomin.94@gmail.com>
2021-06-01 15:01:49 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,7 +3,7 @@ from os import getenv
 IS_PROD = bool(getenv('IS_PROD', False))
 
 BOT_TOKEN = getenv('BOT_TOKEN')
-WEBHOOK_PATH = getenv('WEBHOOK_PATH', '/zmanim')
+WEBHOOK_PATH = getenv('WEBHOOK_PATH', '/my_bot')
 
 METRICS_DSN = getenv('METRICS_DSN')
 METRICS_TABLE_NAME = getenv('METRICS_TABLE_NAME')
